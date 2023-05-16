47544ac6251dc35f8514905c48e4f6d34eb6bb00
kaoru.ohtaka <ockacru+git@gmail.com>
2023-05-17 02:28:37 +0900

    can remove null values

--- packages/pnpm-license-exporter/src/pnpm/export-license/utils/search-license.util.ts
+++ packages/pnpm-license-exporter/src/pnpm/export-license/utils/search-license.util.ts
@@ -6,19 +6,17 @@ type PackageIfHasLicense = PnpmPackageWithLicenseTxt | PnpmPackageInfo;
 
 export function searchLicense(packages: PnpmPackageInfo[]) {
   return Promise.all(
-    packages
-      .map(async (pnpmPackage): Promise<PackageIfHasLicense | undefined> => {
-        const dirents = await readdir(pnpmPackage.path, { withFileTypes: true });
-        const licenseDirent = dirents.find((dirent) => dirent.name.toLowerCase().includes('license'));
-        if (licenseDirent === undefined) {
-          return undefined;
-        }
-        const licenseTxt = await readLicense({
-          name: pnpmPackage.name,
-          licensePath: `${pnpmPackage.path}/${licenseDirent.name}`,
-        });
-        return licenseTxt === undefined ? pnpmPackage : { ...pnpmPackage, licenseTxt };
-      })
-      .filter(Boolean),
-  ) as Promise<PackageIfHasLicense[]>;
+    packages.map(async (pnpmPackage): Promise<PackageIfHasLicense | undefined> => {
+      const dirents = await readdir(pnpmPackage.path, { withFileTypes: true });
+      const licenseDirent = dirents.find((dirent) => dirent.name.toLowerCase().includes('license'));
+      if (licenseDirent === undefined) {
+        return undefined;
+      }
+      const licenseTxt = await readLicense({
+        name: pnpmPackage.name,
+        licensePath: `${pnpmPackage.path}/${licenseDirent.name}`,
+      });
+      return licenseTxt === undefined ? pnpmPackage : { ...pnpmPackage, licenseTxt };
+    }),
+  ).then((licenses) => licenses.filter(Boolean));
 }
