697436a334137b17ac80fd76cc66d9a7a7acfe48
Taiyu Yoshizawa <taiyu31@gmail.com>
2024-04-13 05:36:23 +0900

    Binary support (add Commands) (#30)
    * fix: lockfiles
    
    * feat: add binary pnpm-license-exporter
    
    * update node version

--- src/bin/pnpm-license-exporter.ts
+++ src/bin/pnpm-license-exporter.ts
@@ -1,7 +1,45 @@
 #! /usr/bin/env node
 
 import { execSync } from 'node:child_process';
+import { parseArgs, ParseArgsConfig } from 'node:util';
 import { writeLicenses } from '../pnpm/export-license';
+
+export const argsOptions = {
+  output: {
+    type: 'string',
+    multiple: false,
+    short: 'o',
+    default: './licenses.json',
+  },
+  pretty: {
+    type: 'boolean',
+    multiple: false,
+    short: 'p',
+    default: false,
+  },
+  recursive: {
+    type: 'boolean',
+    multiple: false,
+    short: 'r',
+    default: false,
+  },
+  dev: {
+    type: 'boolean',
+    multiple: false,
+    short: 'd',
+    default: false,
+  },
+  'no-prod': {
+    type: 'boolean',
+    multiple: false,
+    default: false,
+  },
+  'no-optional': {
+    type: 'boolean',
+    multiple: false,
+    default: false,
+  },
+} satisfies ParseArgsConfig['options'];
 
 const nodeLinkerSetting = (
   JSON.parse(execSync('pnpm config list --json').toString('utf8')) as {
@@ -14,5 +52,13 @@ if (nodeLinkerSetting === 'hoisted') {
     '\u001B[33mWARN\u001B[0m: Skipping license export because node-linker is set to hoisted'
   );
 } else {
-  writeLicenses('./licenses.json');
+  const parsedArgs = parseArgs({ options: argsOptions });
+  writeLicenses({
+    output: parsedArgs.values.output,
+    pretty: parsedArgs.values.pretty,
+    recursive: parsedArgs.values.recursive,
+    dev: parsedArgs.values.dev,
+    noProd: parsedArgs.values['no-prod'],
+    noOptional: parsedArgs.values['no-optional'],
+  });
 }
--- src/pnpm/export-license/utils/write-licenses.ts
+++ src/pnpm/export-license/utils/write-licenses.ts
@@ -1,9 +1,38 @@
-import { writeFileSync } from 'node:fs';
+import { writeFile, mkdir } from 'node:fs/promises';
 
 import { generateFlatPnpmPackages } from './generate-flat-pnpm-packages.util';
 import { searchLicense } from './search-license.util';
+import { argsOptions } from '../../../bin/pnpm-license-exporter';
+import path from 'node:path';
 
-export async function writeLicenses(licensesPath: string) {
-  const licenses = await searchLicense(generateFlatPnpmPackages());
-  writeFileSync(licensesPath, JSON.stringify(licenses, null, 2));
+type argType = {
+  output?: string;
+  pretty?: boolean;
+  recursive?: boolean;
+  dev?: boolean;
+  noProd?: boolean;
+  noOptional?: boolean;
+};
+
+export async function writeLicenses({
+  output = argsOptions.output.default,
+  pretty = argsOptions.pretty.default,
+  recursive = argsOptions.recursive.default,
+  dev = argsOptions.dev.default,
+  noProd = argsOptions['no-prod'].default,
+  noOptional = argsOptions['no-optional'].default,
+}: argType) {
+  const licenses = await searchLicense(
+    generateFlatPnpmPackages({
+      recursive,
+      dev,
+      noProd,
+      noOptional,
+    })
+  );
+  const dirName = path.dirname(output);
+  if (dirName) {
+    await mkdir(dirName, { recursive: true });
+  }
+  await writeFile(output, JSON.stringify(licenses, undefined, pretty ? 2 : undefined));
 }
