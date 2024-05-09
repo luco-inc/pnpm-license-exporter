20f9d01ea7dfb5a94c40859481952dda04e99bed
NEKOYASAN <hello@nekoyasan.me>
2024-05-09 15:58:05 +0900

    fix

--- src/pnpm/export-license/utils/find-license-dirent.ts
+++ src/pnpm/export-license/utils/find-license-dirent.ts
@@ -2,6 +2,9 @@ import type { Dirent } from 'node:fs';
 import { readdir } from 'node:fs/promises';
 
 export async function findLicenseDirent(pnpmPackagePath: string): Promise<Dirent | undefined> {
+  if (!pnpmPackagePath) {
+    return undefined;
+  }
   const dirents = await readdir(pnpmPackagePath, { withFileTypes: true });
   return dirents.find((dirent) => dirent.name.toLowerCase().includes('license'));
 }
